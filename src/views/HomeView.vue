47c1fa18e532ebcb61cb74e936aad13e04d4f0d2
Valentina De Mite <valesakura97@gmail.com>
2023-11-22 22:28:36 +0100

    added link discover more

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,24 @@ export default {
 </script>
 
 <template>
-  <h1>This is the Home!!</h1>
+  <div class="container d-flex justify-content-center py-5">
+    <div class="card p-5 rounded shadow">
+      <h1 class="my-4">HI, i'm Valentina!!</h1>
+
+      <button
+        class="rounded btn w-50 m-auto btn-primary bg-violet"
+        type="button"
+      >
+        <router-link class="nav-link light" to="/projects"
+          >Discover More!</router-link
+        >
+      </button>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bg-violet {
+  background-color: rgba(29, 9, 53, 255) !important;
+}
+</style>
